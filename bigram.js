96373cf115dbeabc31640502aeda5ca8f66326ce
Taylor <taylorkkaufman@gmail.com>
2024-08-28 11:28:10 -0600

    initial exercise completion

---
+++ bigram.js
@@ -0,0 +1,58 @@
+// Textual Markov chain generator using bigrams /
+
+class MarkovMachine {
+
+    // Build Markov machine, read in text.
+
+    constructor(text) {
+        let words = text.split(/[\r\n]+/);
+        this.words = words.filter(c => c !== "");
+        this.makeChains();
+    }
+
+    // Set Markov chains: 
+//        *  for text of "the cat in the hat", chains will be
+//    *  {"the cat": ["in"], "cat in": ["the"], "in the": ["hat"], "the hat": [null]} */
+    // //                                          
+    makeChains() {
+        let chains = new Map();
+
+        for (let i = 0; i < this.words.length - 1; i += 1) {
+            let bigram = this.words[i + 1];
+            let nextWord = this.words[i + 2] || null;
+
+            if (chains.has(bigram)) chains.get(bigram).push(nextWord);
+            else chains.set(bigram, [nextWord]);
+        }
+        this.chains = chains;
+    }
+
+    // Pick random choice from array
+
+    choice(ar) {
+        return ar[Math.floor(Math.random() * ar.length)];
+    }
+
+    // Return random text from chains
+
+    makeText(numWords = 100) {
+        // Pick a random key to begin
+        let keys = Array.from(this.chains.keys());
+        let key = this.choice(keys);
+        let out = [];
+
+        // Produce markov chain until reaching termination word
+
+        while (out.length <= numWords && key !== null) {
+            let [w1, w2] = key.split(" ");
+            out.push(w1);
+            key = w2 + " " + this.choice(this.chains.get(key));
+        }
+
+        return out.join(" ");
+    }
+}
+
+module.exports = {
+    MarkovMachine,
+};
